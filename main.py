c40a07f52654a6fa81f25b45152ef5c1d0a2b46d
michaelgailling <michaelgailling@users.noreply.github.com>
2023-04-18 18:11:53 -0400

    A little re-organization

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import json
 import os
 import openai
+import time
+
 
 from UniversalHandbook.prompts.system_prompts import sp_list, sp_detail
 from apikeys import open_ai_key
@@ -14,7 +16,43 @@ exit_commands = [
 ]
 
 
-def make_initial_req(message: str):
+def input_to_list():
+    while True:
+        usr_inp = input("Prompt: ")
+
+        lower_inp = usr_inp.lower()
+
+        if lower_inp in exit_commands:
+            exit()
+
+        if not usr_inp:
+            print("Empty prompts are skipped.")
+            continue
+
+        resp_dict = get_summary_list(usr_inp)
+
+        break
+    return resp_dict
+
+
+def get_summary_list(usr_inp: str):
+    resp_dict = {}
+    while True:
+        resp = make_list_req(usr_inp)
+
+        print(resp)
+
+        try:
+            resp_dict = json.loads(resp)
+        except:
+            print("GPT returned poorly formatted JSON.")
+            time.sleep(1)
+            continue
+        break
+    return resp_dict
+
+
+def make_list_req(message: str):
     history = [{"role": "system", "content": sp_list},
                {"role": "user", "content": message}]
 
@@ -25,10 +63,25 @@ def make_initial_req(message: str):
                 messages=history
             )
         except:
+            print("OpenAI had a problem.")
+            time.sleep(1)
             continue
         break
 
     return resp["choices"][0]["message"]["content"]
+
+
+def get_step_detail(resp_dict: dict):
+    objective = resp_dict["instructions"][0]
+
+    for i in range(1, len(resp_dict["instructions"])):
+        step: dict = resp_dict["instructions"][i]
+
+        step_n = list(step.keys())
+        step_n = step_n[0]
+        step_d = step[step_n]
+
+        print(make_detail_req(objective=objective, step_n=step_n, step_d=step_d))
 
 
 def make_detail_req(objective: str, step_n: str, step_d: str):
@@ -43,55 +96,26 @@ def make_detail_req(objective: str, step_n: str, step_d: str):
                 messages=history
             )
         except:
+            print("OpenAI had a problem.")
+            time.sleep(1)
             continue
         break
 
     return resp["choices"][0]["message"]["content"]
 
 
-def get_summary_list(usr_inp: str):
-    resp_dict = {}
-    while True:
-        resp = make_initial_req(usr_inp)
-
-        print(resp)
-
-        try:
-            resp_dict = json.loads(resp)
-        except:
-            print("Something went wrong!")
-            continue
-        break
-    return resp_dict
 
 
-resp_dict = {}
 
-while True:
-    usr_inp = input("Prompt: ")
 
-    lower_inp = usr_inp.lower()
 
-    if lower_inp in exit_commands:
-        exit()
 
-    if not usr_inp:
-        print("Empty prompts are skipped.")
-        continue
 
-    resp_dict = get_summary_list(usr_inp)
 
-    break
+resp_dict = input_to_list()
 
-n = len(resp_dict["instructions"])
+get_step_detail(resp_dict)
 
-objective = resp_dict["instructions"][0]
 
-for i in range(1, n):
-    step: dict = resp_dict["instructions"][i]
 
-    step_n = list(step.keys())
-    step_n = step_n[0]
-    step_d = step[step_n]
 
-    print(make_detail_req(objective=objective, step_n=step_n, step_d=step_d))
